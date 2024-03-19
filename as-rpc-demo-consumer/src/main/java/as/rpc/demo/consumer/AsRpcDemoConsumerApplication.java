016f43dda6e32872b60dc5aee305f8a3aeb4a323
abner song <songkd90@163.com>
2024-03-19 18:20:35 +0800

    practice 2: add consumer demo

---
+++ as-rpc-demo-consumer/src/main/java/as/rpc/demo/consumer/AsRpcDemoConsumerApplication.java
@@ -0,0 +1,13 @@
+package as.rpc.demo.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsRpcDemoConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsRpcDemoConsumerApplication.class, args);
+    }
+
+}
---
+++ as-rpc-demo-consumer/src/test/java/as/rpc/demo/consumer/AsRpcDemoConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package as.rpc.demo.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsRpcDemoConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
